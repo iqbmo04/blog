1a545d8c60612056fd08062dcde7c58e033ccadb
Ashish Singh Baghel <mechashishsingh@gmail.com>
2022-04-17 20:46:39 +0000

    Use gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: `Blogging Website`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Blogging Website by Ashish Singh Baghel`,
+    siteUrl: `https://www.techhackswithash.com`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-react-helmet", 
+    "gatsby-plugin-sitemap", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, 
+    "gatsby-plugin-mdx", 
+    "gatsby-transformer-remark", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      }, 
+      __key: "images" 
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      }, 
+      __key: "pages"
     }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  ] 
 };
